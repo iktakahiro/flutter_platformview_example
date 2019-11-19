b15bb4019f7306f78a118f817b584d332e977873
iktakahiro <takahiro.ikeuchi@gmail.com>
2019-11-19 14:17:31 +0900

    Implement FIrstWidget

---
+++ lib/first.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+typedef void FirstWidgetCreatedCallback(FirstWidgetController controller);
+
+class FirstWidget extends StatefulWidget {
+  const FirstWidget({
+    Key key,
+    this.onFirstWidgetWidgetCreated,
+  }) : super(key: key);
+
+  final FirstWidgetCreatedCallback onFirstWidgetWidgetCreated;
+
+  @override
+  State<StatefulWidget> createState() => _FirstWidgetState();
+}
+
+class _FirstWidgetState extends State<FirstWidget> {
+  @override
+  Widget build(BuildContext context) {
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      return AndroidView(
+        viewType: 'plugins/first_widget',
+        onPlatformViewCreated: _onPlatformViewCreated,
+        creationParamsCodec: const StandardMessageCodec(),
+      );
+    }
+    return const Text('iOS platform version is not implemented yet.');
+  }
+
+  void _onPlatformViewCreated(int id) {
+    if (widget.onFirstWidgetWidgetCreated == null) {
+      return;
+    }
+    widget.onFirstWidgetWidgetCreated(FirstWidgetController._(id));
+  }
+}
+
+class FirstWidgetController {
+  FirstWidgetController._(int id)
+      : _channel = MethodChannel('plugins/first_widget_$id');
+
+  final MethodChannel _channel;
+
+  Future<void> ping() async {
+    return _channel.invokeMethod('ping');
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import './first.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter PlatformView Example'),
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: const <Widget>[
+          Card(
+            child: SizedBox(
+              height: 200,
+              child: FirstWidget(),
+            ),
+          ),
+          Text('second'),
+        ],
+      ),
+    );
+  }
+}
